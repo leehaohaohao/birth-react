76a2b2a9f3106942ce5b38739d4005010775a8ed
lihao <805459342@qq.com>
2025-02-25 23:06:47 +0800

    -feat: 首页

---
+++ src/component/HomePage.tsx
@@ -0,0 +1,28 @@
+/**
+ * @description
+ * @author lihao
+ * @date 2025/2/23 23:29
+ */
+import './HomePage.css';
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <div className="birthday-message">
+                <h1>生日快乐！🎉</h1>
+                <p>今天是属于你的特别日子，希望你每一天都能如星般闪耀。</p>
+            </div>
+
+            <div className="nav-blocks">
+                <div className="nav-item">嘿嘿</div>
+                <div className="nav-item">cake</div>
+                <div className="nav-item">祝福</div>
+                <div className="nav-item">关于</div>
+            </div>
+        </div>
+    );
+};
+
+export default HomePage;
+
+
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import HomePage from "./component/HomePage.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HomePage />
   </StrictMode>,
 )
